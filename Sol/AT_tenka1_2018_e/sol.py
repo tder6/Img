433e9962a212b8cc5aff813bec48a18ba4c889ed
tder <1218546874@qq.com>
2024-11-28 20:31:37 +0800

    add

--- Sol/AT_tenka1_2018_e/sol.py
+++ Sol/AT_tenka1_2018_e/sol.py
@@ -2,7 +2,7 @@ path = "D:\\Desktop\\Project\\Img\\"
 import sys; sys.path.append(path + "Sol\\template"); from seg import *
 
 x1, y1 = -1, 1; x2, y2 = 0, -2; x3, y3 = 2, 0
-eps = 0.25
+eps1, eps2 = 0.25, 0.35
 
 class Sol(Scene):
 	def construct(self):
@@ -12,9 +12,12 @@ class Sol(Scene):
 			Line([x1, y1, 0], [x2, y1, 0], color = BLACK),
 			Line([x2, y2, 0], [x2, y1, 0], color = BLACK),
 			Line([x3, y3, 0], [x2, y3, 0], color = BLACK),
-			Tex(r"$a$", color = BLACK).move_to([(x1 + x2) / 2, y1 + eps, 0]).scale(0.8),
-			Tex(r"$b$", color = BLACK).move_to([x2 - eps, (y1 + y3) / 2, 0]).scale(0.8),
-			Tex(r"$c$", color = BLACK).move_to([x2 - eps, (y2 + y3) / 2, 0]).scale(0.8),
-			Tex(r"$d$", color = BLACK).move_to([(x2 + x3) / 2, y3 - eps, 0]).scale(0.8),
+			Tex(r"$a$", color = BLACK).move_to([(x1 + x2) / 2, y1 + eps1, 0]).scale(0.8),
+			Tex(r"$b$", color = BLACK).move_to([x2 - eps1, (y1 + y3) / 2, 0]).scale(0.8),
+			Tex(r"$c$", color = BLACK).move_to([x2 - eps1, (y2 + y3) / 2, 0]).scale(0.8),
+			Tex(r"$d$", color = BLACK).move_to([(x2 + x3) / 2, y3 - eps1, 0]).scale(0.8),
+			Tex(r"$A$", color = BLACK).move_to([x1 - eps2, y1, 0]).scale(0.8),
+			Tex(r"$B$", color = BLACK).move_to([x2, y2 - eps2, 0]).scale(0.8),
+			Tex(r"$A$", color = BLACK).move_to([x3 + eps2, y3, 0]).scale(0.8),
 		]
 		add(self, list)
